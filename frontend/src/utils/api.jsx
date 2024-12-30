0f7aa2e566a72cdadf994fe9b931fba15bee37c9
Mithun Yadav <itsmithun01@gmail.com>
2024-12-30 17:50:54 +0530

    Update api.jsx

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://quindecim.up.railway.app/api'
+  baseURL: import.meta.env.VITE_API_URL || 'https://www.senpailist.site/api'
 })
 
 api.setAuthToken = (token) => {
